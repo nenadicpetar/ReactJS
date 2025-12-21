73763ba748eec466719e9c520e70744b3a418344
nenadicpetar <nenadic.petar2@gmail.com>
2025-12-21 09:23:26 +0100

    commit 21122025

---
+++ E_signature_app/e_signature/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Esignatureapp from "./projects/01-e-signature-app/Esignatureapp";
+
+function App() {
+  return (
+    <div className="App">
+      <Esignatureapp />
+    </div>
+  );
+}
+
+export default App;
